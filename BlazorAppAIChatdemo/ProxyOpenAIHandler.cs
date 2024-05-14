b9f434c7183f3997f648571b92ef6d659171fb45
kartik <kartik.kumar53@gmail.com>
2024-05-14 09:44:58 +0530

    pushchnage

--- BlazorAppAIChatdemo/ProxyOpenAIHandler.cs
+++ BlazorAppAIChatdemo/ProxyOpenAIHandler.cs
@@ -7,6 +7,7 @@
             if (request.RequestUri != null && request.RequestUri.Host.Equals("api.openai.com", StringComparison.OrdinalIgnoreCase))
             {
                 // your proxy url
+                //
                 request.RequestUri = new Uri($"http://localhost:3333{request.RequestUri.PathAndQuery}");
             }
             return base.SendAsync(request, cancellationToken);
